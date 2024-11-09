ce00a99db224e4c54cf3787065f6010586339661
Ilya <ilya671qw@gmail.com>
2024-11-09 15:58:33 +0300

    drop

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Posts from "./components/Posts";
+import MyProfilePic from "./components/MyProfilePic";
 
 export const revalidate = 10
 
 export default function Home() {
   return (
-    <main className="px-6 mx-auto">
+    <div className="mx-auto">
+      <MyProfilePic />
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Welcome 
         <span className="whitespace-nowrap">
@@ -12,6 +14,6 @@ export default function Home() {
         </span>
       </p>
       <Posts />
-    </main>
+    </div>
   );
 }
